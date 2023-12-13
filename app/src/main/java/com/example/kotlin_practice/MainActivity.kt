20fcdb159b0912f5260504801f5c777f5cea313b
SOTAkkkk <>
2023-12-13 22:06:03 +0900

    4. Compose 象限

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.kotlin_practice
 
+import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -45,39 +49,71 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.ic_task_completed)
-
-    Column (
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Image(
-            painter = image,
-            contentDescription = null
-        )
-        Text(
-            text = "All tasks completed",
-            textAlign = TextAlign.End,
-            fontWeight = FontWeight.Bold,
-            modifier = modifier.padding(
-                top = 24.dp,
-                bottom = 8.dp
+    ) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = "Text composable",
+                description = "Displays text and follows the recommended Material Design guidelines.",
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
             )
-        )
-        Text(
-            text = "Nice work!",
-            fontSize = 16.sp,
-            modifier = modifier.padding(
-                vertical = 0.dp,
-                horizontal = 16.dp
+            ComposableInfoCard(
+                title = "Image composable\n",
+                description = "Creates a composable that lays out and draws a given Painter class object.\n",
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
             )
-        )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = "Text composable",
+                description = "Displays text and follows the recommended Material Design guidelines.\n",
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = "Column composable",
+                description = "A layout composable that places its children in a vertical sequence.",
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+
     }
 }
 
+@Composable
+private fun ComposableInfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
